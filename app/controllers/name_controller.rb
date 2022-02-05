48bb13035a3ad08b6bd3a39e5edec623cd6d9dba
PotatoParser <45542237+PotatoParser@users.noreply.github.com>
2022-02-04 23:41:39 -0800

    First commit

---
+++ app/controllers/name_controller.rb
@@ -0,0 +1,6 @@
+class NameController < ApplicationController
+  def show
+    names = ["Debbie Yuen", "Ethan Bradley", "Julian Bellavita", "Justin Cheng", "Wilson Nguyen", "Kersten Naab"]
+    @name = names.sample(1)[0]
+  end
+end
